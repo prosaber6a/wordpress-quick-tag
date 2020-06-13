8ac7bbdcf15c4eb3054bb920e14880e97f368c93
Saber Hossen Rabbani <saber.miazy@gmail.com>
2020-06-13 16:09:38 +0600

    first quick tag added

--- assets/admin/js/quick-tag.js
+++ assets/admin/js/quick-tag.js
@@ -1,9 +1,2 @@
-// alert("hello")
-QTags.addButton('quick-tag-button-one', 'U', '<u>', '</u>');
-QTags.addButton('quick-tag-button-two', 'JS', jsPropmpt);
 
-function jsPropmpt () {
-	var name = prompt("What's your name??");
-	// var text = name;
-	QTags.insertContent(name);
-}
+QTags.addButton('quick-tag-button-one', 'U', '<u>', '</u>');
