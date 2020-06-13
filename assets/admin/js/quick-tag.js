97b96be16efc02775e247653142c92eaa139d435
Saber Hossen Rabbani <saber.miazy@gmail.com>
2020-06-13 16:10:58 +0600

    js prompt value functionalty added

--- assets/admin/js/quick-tag.js
+++ assets/admin/js/quick-tag.js
@@ -1,2 +1,9 @@
 
 QTags.addButton('quick-tag-button-one', 'U', '<u>', '</u>');
+QTags.addButton('quick-tag-button-two', 'JS', jsPropmpt);
+
+function jsPropmpt () {
+	var name = prompt("What's your name??");
+	// var text = name;
+	QTags.insertContent(name);
+}
